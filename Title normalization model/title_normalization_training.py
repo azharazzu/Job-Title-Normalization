4ec859631b270da050201e31767cd446bd55b63d
azharazzu <101692969+azharazzu@users.noreply.github.com>
2023-03-28 10:16:06 +0530

    Update and rename Career_pathway_matrices_sbo.ipynb to title_normalization_training.py

---
+++ Title normalization model/title_normalization_training.py
@@ -0,0 +1,44 @@
+mport numpy as np
+import pandas as pd
+import time
+import os
+from tqdm import tqdm
+import logging
+from simpletransformers.seq2seq import (
+    Seq2SeqModel,
+    Seq2SeqArgs,
+)
+
+
+logging.basicConfig(level=logging.INFO)
+transformers_logger = logging.getLogger("transformers")
+transformers_logger.setLevel(logging.WARNING)
+
+cuda_available = torch.cuda.is_available()
+
+
+model_args = Seq2SeqArgs()
+model_args.num_train_epochs = 3
+model_args.train_batch_size=64
+model_args.no_save = False
+model_args.evaluate_generated_text = True
+model_args.evaluate_during_training = True
+model_args.evaluate_during_training_verbose = True
+model_args.use_multiprocessing=False
+model_args.overwrite_output_dir=True
+model_args.save_model_every_epoch=True
+# model_args.thread_count=8
+# model_args.n_gpu=6
+
+
+model = Seq2SeqModel(
+    encoder_decoder_type="bart",
+    encoder_decoder_name="facebook/bart-large",
+    args=model_args,
+    use_cuda=False,
+)
+
+
+model.train_model(
+    train_df, eval_data=eval_df, matches=count_matches
+)
